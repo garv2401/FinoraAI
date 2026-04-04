f59488b5bcff734e5fe90ae6a2c70c6a6ed086a6
GARVIT <garvitkumar504@gmail.com>
2026-04-05 00:05:32 +0530

    Feat:Configured Report Settings Module

--- client/src/pages/reports/_component/report-table.tsx
+++ client/src/pages/reports/_component/report-table.tsx
@@ -1,51 +1,43 @@
-import { DataTable } from '@/components/data-table'
-import { REPORT_DATA } from './data'
-import { reportColumns } from './column'
-import { useState } from 'react'
+import { DataTable } from "@/components/data-table";
+import { reportColumns } from "./column";
+import { useState } from "react";
+import { useGetAllReportsQuery } from "@/features/report/reportAPI";
 
 const ReportTable = () => {
   const [filter, setFilter] = useState({
     pageNumber: 1,
     pageSize: 10,
-  })
+  });
 
-  // const { data, isFetching } = useGetAllReportsQuery(filter)
-
-  // const pagination = {
-  //   totalItems: data?.pagination?.totalCount || 0,
-  //   totalPages: data?.pagination?.totalPages || 0,
-  //   pageNumber: filter.pageNumber,
-  //   pageSize: filter.pageSize,
-  // }
+  const { data, isFetching } = useGetAllReportsQuery(filter);
 
   const pagination = {
-    totalItems:  0,
-    totalPages:  0,
+    totalItems: data?.pagination?.totalCount || 0,
+    totalPages: data?.pagination?.totalPages || 0,
     pageNumber: filter.pageNumber,
     pageSize: filter.pageSize,
-  }
+  };
 
   const handlePageChange = (pageNumber: number) => {
-    setFilter((prev) => ({ ...prev, pageNumber }))
-  }
+    setFilter((prev) => ({ ...prev, pageNumber }));
+  };
 
   const handlePageSizeChange = (pageSize: number) => {
-    setFilter((prev) => ({ ...prev, pageSize }))
-  }
-
+    setFilter((prev) => ({ ...prev, pageSize }));
+  };
 
   return (
     <DataTable
-        data={REPORT_DATA} //data?.reports || []
-        columns={reportColumns}
-        isLoading={false}
-        showSearch={false}
-        className="[&_td]:!w-[5%]"
-        pagination={pagination}
-        onPageChange={handlePageChange}
-        onPageSizeChange={handlePageSizeChange}
-      />
-  )
-}
+      data={data?.reports || []} //data?.reports || []
+      columns={reportColumns}
+      isLoading={isFetching}
+      showSearch={false}
+      className="[&_td]:!w-[5%]"
+      pagination={pagination}
+      onPageChange={handlePageChange}
+      onPageSizeChange={handlePageSizeChange}
+    />
+  );
+};
 
-export default ReportTable
+export default ReportTable;
